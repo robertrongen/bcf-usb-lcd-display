79afca633cbf49c67a4a9082b13cf0ff9fb1fe98
Robert Rongen <robert.rongen@blockchainadvies.nu>
2020-01-03 23:03:20 +0100

    Update usb-sender.py

--- python/usb-sender.py
+++ python/usb-sender.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import PIL
-import Image, ImageFont, ImageDraw
+from PIL import Image, ImageFont, ImageDraw
 import binascii
 import base64
 #import pyautogui
