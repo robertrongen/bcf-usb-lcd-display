958a3237e5aa5f80a0fb9ca35d29a666b23c46fc
Robert Rongen <37987463+robertrongen@users.noreply.github.com>
2020-01-06 21:32:20 +0100

    modified:   python/qr-sender.py

--- python/qr-sender.py
+++ python/qr-sender.py
@@ -77,10 +77,9 @@ orderId = 1
 
 def screen_draw():
     # Print QR code with Order ID
-    img = Image.open("qr-code-logo-1.png")
-    img = img.resize((128, 128))
-    if type(img) == int:
-        img = (img,img,img)
+    #img = Image.open("qr-code-logo-1.png")
+    #img = img.resize((128, 128))
+    img = Image.new("RGB", (128, 128), (255,255,255))
     #img = qrImage(orderId)
     return (img, 0.1)
 
